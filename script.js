f7e6b9bb32216602ef25ce073a2d04af60888554
kushik00 <lada.chokol@gmail.com>
2024-04-28 19:26:14 +0300

    added new chenges

--- script.js
+++ script.js
@@ -9,3 +9,22 @@ function closeModal() {
     modalContainer.classList.remove('show');
     document.body.style.overflow = ''; // Восстановление прокрутки фона
 }
+
+let offset = 0;
+const sliderLine = document.querySelector('.slider-line');
+
+document.querySelector('.slider-next').addEventListener('click', function(){
+    offset = offset + 1390;
+    if (offset > 6600) {
+        offset = 0;
+    }
+    sliderLine.style.left = -offset + 'px';
+});
+
+document.querySelector('.slider-prev').addEventListener('click', function () {
+    offset = offset - 1390;
+    if (offset < 0) {
+        offset = 0;
+    }
+    sliderLine.style.left = -offset + 'px';
+});
